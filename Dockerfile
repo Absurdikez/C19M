59782e779c603a46e5821eeb8584fb5b1d39d1f7
Zeki Topcu <zekitopccu@gmail.com>
2022-05-11 18:59:08 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+#
+FROM python:3.9
+
+# 
+WORKDIR /code
+
+# 
+COPY ./requirements.txt /code/requirements.txt
+
+# 
+RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+
+# 
+COPY ./app /code/app 
+
+#
+COPY ./static /code/static
+
+#
+
+COPY ./art-museum-master /code/art-museum-master
+
+
+# 
+CMD ["uvicorn", "app.mainv2:app", "--host", "0.0.0.0", "--port", "80"]
---
+++ app/main.py
@@ -0,0 +1,41 @@
+from operator import lt
+from pathlib import Path
+import uvicorn
+
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.responses import HTMLResponse
+import shutil
+from fastapi.staticfiles import StaticFiles
+import os
+app = FastAPI()
+
+os.mkdir('files')
+app.mount("/files", StaticFiles(directory="files"), name="files") #to make a file static
+
+@app.post("/uploadfile")
+async def upload_file(file: UploadFile = File(...,description="Upload file"), author_name: str = Form(...,max_length=50,description="Author name"),
+ keywords: str = Form(...,description="Enter keywords")):
+	print(author_name)
+	file_location = f"files/{file.filename}"
+	with open(file_location, "wb") as buffer:
+		shutil.copyfileobj(file.file, buffer)
+	return {"info": f"file '{file.filename}' saved at '{file_location}'"}
+
+# author_name: str = Form(...), keywords: str = Form(...)
+
+@app.get("/")
+async def main():
+	content = """
+<body>
+	<form action="/uploadfile" enctype="multipart/form-data" method="post">
+		<input name="file" type="file">
+		<input name="author_name">
+		<input name="keywords">
+		<input type="submit">
+	</form>
+</body>
+"""
+	return HTMLResponse(content=content)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
